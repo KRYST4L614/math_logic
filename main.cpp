92773a0e83327bf560d1f7a07a501713e6bdf8b3
KRYST4L614 <108845539+KRYST4L614@users.noreply.github.com>
2023-11-26 18:13:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 #include <math.h>
 
-const std::size_t N = 9;			// objects count 
-const std::size_t M = 4;			// params  count
-const std::size_t ROW_LENGTH = sqrt(N);	// row length of matrix
+const std::size_t N = 9;													// objects count 
+const std::size_t M = 4;													// params  count
+const std::size_t ROW_LENGTH = sqrt(N);										// row length of matrix
 const std::size_t LOG_N = static_cast<int>(std::ceil(std::log2(N)));		// params values count
-const std::size_t VAR_NUM = N*M*LOG_N;	// count of bool var
-char* var = new char[VAR_NUM];					// bool var array
+const std::size_t VAR_NUM = N*M*LOG_N;										// count of bool var
+char* var = new char[VAR_NUM];												// bool var array
 
 void print()
 {
@@ -88,10 +88,12 @@ void cond1(bdd& tree, const bdd p[M][N][N])
   tree &= p[2][5][5];
   tree &= p[1][6][5];
   //additional constraints
-  tree &= p[3][5][4];
-  tree &= p[3][8][6];
-  tree &= p[0][2][5];
+  tree &= p[1][4][6];
+  tree &= p[3][2][0];
   tree &= p[0][1][4];
+  tree &= p[2][3][0];
+  tree &= p[1][8][7];
+  tree &= p[3][7][8];
 }
 
 void cond2(bdd& tree, const bdd p[M][N][N])
@@ -99,16 +101,20 @@ void cond2(bdd& tree, const bdd p[M][N][N])
   for (std::size_t i = 0; i < N; i++)
   {
 	tree &= !(p[0][i][0] ^ p[3][i][1]);
-	tree &= !(p[1][i][7] ^ p[3][i][3]);
+	tree &= !(p[2][i][7] ^ p[3][i][3]);
 	tree &= !(p[0][i][1] ^ p[1][i][5]);
 	tree &= !(p[1][i][2] ^ p[2][i][5]);
 	//additional constraints
-	tree &= !(p[3][i][0] ^ p[1][i][8]);//48
-	tree &= !(p[0][i][1] ^ p[2][i][1]);//8
+	tree &= !(p[3][i][0] ^ p[1][i][8]);
+	tree &= !(p[0][i][1] ^ p[2][i][1]);
 
-	tree &= !(p[0][i][6] ^ p[1][i][4]);//2
+	tree &= !(p[0][i][6] ^ p[1][i][4]);
 
-	tree &= !(p[0][i][7] ^ p[2][i][3]);//1
+	tree &= !(p[0][i][7] ^ p[2][i][3]);//16
+	tree &= !(p[3][i][1] ^ p[1][i][3]);//8
+	tree &= !(p[2][i][6] ^ p[1][i][1]);//4
+	tree &= !(p[0][i][5] ^ p[1][i][8]);//2
+	tree &= !(p[1][i][5] ^ p[3][i][7]);//1
   }
 }
 
@@ -121,13 +127,6 @@ void cond3(bdd& tree, const bdd p[M][N][N])
 	{
 	  index = i + ROW_LENGTH;
 	  tree &= !(p[1][i][2] ^ p[2][index][4]);
-	  tree &= !(p[0][i][2] ^ p[0][index][7]);
-	}
-	if (i >= N - ROW_LENGTH) //down contraint with gluing
-	{
-	  index = i % ROW_LENGTH;
-	  tree &= !(p[1][i][2] ^ p[2][index][4]);
-	  tree &= !(p[0][i][2] ^ p[0][index][7]);
 	}
 	if ((i + 1) % ROW_LENGTH != 0) //right contraint without gluing
 	{
@@ -135,11 +134,21 @@ void cond3(bdd& tree, const bdd p[M][N][N])
 	  tree &= !(p[0][i][2] ^ p[3][index][4]);
 	  tree &= !(p[0][i][0] ^ p[1][index][1]);
 	}
+	if ((i + 1) % ROW_LENGTH == 0) //right contraint with gluing
+	{
+	  index = i - ROW_LENGTH + 1;
+	  tree &= !(p[0][i][2] ^ p[3][index][4]);
+	  tree &= !(p[0][i][0] ^ p[1][index][1]);
+	}
 	if (i % ROW_LENGTH != 0) //left contraint without gluing
 	{
 	  index = i - 1;
 	  tree &= !(p[2][i][7] ^ p[3][index][5]);
-	  tree &= !(p[3][i][2] ^ p[0][index][0]);
+	}
+	if (i % ROW_LENGTH == 0) //left contraint with gluing
+	{
+	  index = i + ROW_LENGTH - 1;
+	  tree &= !(p[2][i][7] ^ p[3][index][5]);
 	}
   }
 }
@@ -155,34 +164,63 @@ void cond4(bdd& tree, const bdd p[M][N][N])
 	{
 	  top_right_index = i + 1 - ROW_LENGTH;
 	  down_right_index = i + 1 + ROW_LENGTH;
-	  tree &= !(p[3][i][1] ^ p[1][down_right_index][6]) | !(p[3][i][1] ^ p[1][top_right_index][6]);
+	  tree &= !(p[3][i][4] ^ p[1][down_right_index][5]) | !(p[3][i][4] ^ p[1][top_right_index][5]);
 	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]) | !(p[3][i][3] ^ p[0][top_right_index][8]);
-	  tree &= !(p[1][i][0] ^ p[0][down_right_index][5]) | !(p[1][i][0] ^ p[0][top_right_index][5]);
-	  tree &= !(p[2][i][0] ^ p[1][down_right_index][0]) | !(p[2][i][0] ^ p[1][top_right_index][0]);
-	  tree &= !(p[3][i][7] ^ p[0][down_right_index][4]) | !(p[3][i][7] ^ p[0][top_right_index][4]);
-	  tree &= !(p[2][i][6] ^ p[1][down_right_index][7]) | !(p[2][i][6] ^ p[1][top_right_index][7]);
+	  tree &= !(p[0][i][2] ^ p[0][down_right_index][5]) | !(p[0][i][2] ^ p[0][top_right_index][5]);
+	  tree &= !(p[0][i][3] ^ p[0][down_right_index][0]) | !(p[0][i][3] ^ p[0][top_right_index][0]);
+	  tree &= !(p[2][i][7] ^ p[3][down_right_index][6]) | !(p[2][i][7] ^ p[3][top_right_index][6]);
+	  tree &= !(p[3][i][5] ^ p[0][down_right_index][4]) | !(p[3][i][5] ^ p[0][top_right_index][4]);
 	}
-	else if ((i < ROW_LENGTH) && ((i + 1) % ROW_LENGTH != 0)) // top gluing
+	else if (((i + 1) % ROW_LENGTH == 0) && (i < N - ROW_LENGTH) && (i >= ROW_LENGTH)) // all neghbors wigh gluing
 	{
-	  top_right_index = i + N - ROW_LENGTH + 1;
+	  top_right_index = i - ROW_LENGTH - ROW_LENGTH + 1;
+	  down_right_index = i + 1;
+	  tree &= !(p[3][i][4] ^ p[1][down_right_index][5]) | !(p[3][i][4] ^ p[1][top_right_index][5]);
+	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]) | !(p[3][i][3] ^ p[0][top_right_index][8]);
+	  tree &= !(p[0][i][2] ^ p[0][down_right_index][5]) | !(p[0][i][2] ^ p[0][top_right_index][5]);
+	  tree &= !(p[0][i][3] ^ p[0][down_right_index][0]) | !(p[0][i][3] ^ p[0][top_right_index][0]);
+	  tree &= !(p[2][i][7] ^ p[3][down_right_index][6]) | !(p[2][i][7] ^ p[3][top_right_index][6]);
+	  tree &= !(p[3][i][5] ^ p[0][down_right_index][4]) | !(p[3][i][5] ^ p[0][top_right_index][4]);
+	}
+	else if (i < ROW_LENGTH && (i + 1) % ROW_LENGTH != 0) // only down right neighbor without gluing
+	{
 	  down_right_index = i + 1 + ROW_LENGTH;
-	  tree &= !(p[3][i][1] ^ p[1][down_right_index][6]) | !(p[3][i][1] ^ p[1][top_right_index][6]);
-	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]) | !(p[3][i][3] ^ p[0][top_right_index][8]);
-	  tree &= !(p[1][i][0] ^ p[0][down_right_index][5]) | !(p[1][i][0] ^ p[0][top_right_index][5]);
-	  tree &= !(p[2][i][0] ^ p[1][down_right_index][0]) | !(p[2][i][0] ^ p[1][top_right_index][0]);
-	  tree &= !(p[3][i][7] ^ p[0][down_right_index][4]) | !(p[3][i][7] ^ p[0][top_right_index][4]);
-	  tree &= !(p[2][i][6] ^ p[1][down_right_index][7]) | !(p[2][i][6] ^ p[1][top_right_index][7]);
+	  tree &= !(p[3][i][4] ^ p[1][down_right_index][5]);
+	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]);
+	  tree &= !(p[0][i][2] ^ p[0][down_right_index][5]);
+	  tree &= !(p[0][i][3] ^ p[0][down_right_index][0]);
+	  tree &= !(p[2][i][7] ^ p[3][down_right_index][6]);
+	  tree &= !(p[3][i][5] ^ p[0][down_right_index][4]);
 	}
-	else if (i >= N - ROW_LENGTH && ((i + 1) % ROW_LENGTH != 0)) // down gluing
+	else if (i < ROW_LENGTH && (i + 1) % ROW_LENGTH == 0) // only down right neighbor with gluing
 	{
-	  top_right_index = i + 1 - ROW_LENGTH;
-	  down_right_index = i % ROW_LENGTH + 1;
-	  tree &= !(p[3][i][1] ^ p[1][down_right_index][6]) | !(p[3][i][1] ^ p[1][top_right_index][6]);
-	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]) | !(p[3][i][3] ^ p[0][top_right_index][8]);
-	  tree &= !(p[1][i][0] ^ p[0][down_right_index][5]) | !(p[1][i][0] ^ p[0][top_right_index][5]);
-	  tree &= !(p[2][i][0] ^ p[1][down_right_index][0]) | !(p[2][i][0] ^ p[1][top_right_index][0]);
-	  tree &= !(p[3][i][7] ^ p[0][down_right_index][4]) | !(p[3][i][7] ^ p[0][top_right_index][4]);
-	  tree &= !(p[2][i][6] ^ p[1][down_right_index][7]) | !(p[2][i][6] ^ p[1][top_right_index][7]);
+	  down_right_index = i + 1;
+	  tree &= !(p[3][i][4] ^ p[1][down_right_index][5]);
+	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]);
+	  tree &= !(p[0][i][2] ^ p[0][down_right_index][5]);
+	  tree &= !(p[0][i][3] ^ p[0][down_right_index][0]);
+	  tree &= !(p[2][i][7] ^ p[3][down_right_index][6]);
+	  tree &= !(p[3][i][5] ^ p[0][down_right_index][4]);
+	}
+	else if (i >= N - ROW_LENGTH && (i + 1) % ROW_LENGTH != 0) // only top right neighbor without gluing
+	{
+	  down_right_index = i + 1 - ROW_LENGTH;
+	  tree &= !(p[3][i][4] ^ p[1][down_right_index][5]);
+	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]);
+	  tree &= !(p[0][i][2] ^ p[0][down_right_index][5]);
+	  tree &= !(p[0][i][3] ^ p[0][down_right_index][0]);
+	  tree &= !(p[2][i][7] ^ p[3][down_right_index][6]);
+	  tree &= !(p[3][i][5] ^ p[0][down_right_index][4]);
+	}
+	else if (i >= N - ROW_LENGTH && (i + 1) % ROW_LENGTH == 0) // only top right neighbor with gluing
+	{
+	  down_right_index = i + 1 - ROW_LENGTH - ROW_LENGTH;
+	  tree &= !(p[3][i][4] ^ p[1][down_right_index][5]);
+	  tree &= !(p[3][i][3] ^ p[0][down_right_index][8]);
+	  tree &= !(p[0][i][2] ^ p[0][down_right_index][5]);
+	  tree &= !(p[0][i][3] ^ p[0][down_right_index][0]);
+	  tree &= !(p[2][i][7] ^ p[3][down_right_index][6]);
+	  tree &= !(p[3][i][5] ^ p[0][down_right_index][4]);
 	}
   }
 }
@@ -240,10 +278,7 @@ int main()
 
   std::size_t satcount = bdd_satcount(tree);
   std::cout << "Found " << satcount << " solutions\n\n";
-  if (satcount)
-  {
-	bdd_allsat(tree, fun);
-  }
+  bdd_allsat(tree, fun);
   bdd_done();
   delete[] var;
   return 0;
